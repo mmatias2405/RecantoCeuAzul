73c7fdc949d7c2705faa7619231ac1ca78a54fc9
Matheus Matias <pt242512@ifsproot.ptb>
2025-08-06 13:44:05 -0300

    Projeto reorganizado em containers, api e web no mesmo diretório

---
+++ web/src/main/java/com/recantoceuazul/web/controller/HomeController.java
@@ -0,0 +1,64 @@
+package com.recantoceuazul.web.controller;
+
+import com.recantoceuazul.web.model.Abastecimento;
+import com.recantoceuazul.web.model.Administrador;
+import com.recantoceuazul.web.model.Setor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Value("${api.url}")
+    private String API_URL;
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("/")
+    public String index(Model model) {
+        ResponseEntity<Abastecimento[]> response = restTemplate.getForEntity(API_URL + "abastecimentos", Abastecimento[].class);
+        List<Abastecimento> abastecimentos = Arrays.asList(response.getBody());
+        model.addAttribute("abastecimentos", abastecimentos);
+        ResponseEntity<Setor[]> responseSetor = restTemplate.getForEntity(API_URL + "setor", Setor[].class);
+        List<Setor> setores = Arrays.asList(responseSetor .getBody());
+        model.addAttribute("setores", setores);
+
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+    @GetMapping("/consumo")
+    public String consumo(Model model) {
+        return "registrarConsumo";
+    }
+
+    @GetMapping("/captacao")
+    public String captacao(Model model) {
+        return "registrarCaptacao";
+    }
+
+    @GetMapping("/setor")
+    public String setor(Model model) {
+        return "registrarSetor";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(@ModelAttribute Administrador administrador) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Administrador> request = new HttpEntity<>(administrador, headers);
+        restTemplate.postForEntity(API_URL, request, Administrador.class);
+        return "redirect:/";
+    }
+}
--- web/src/test/java/com/recantoceuazul/web/WebApplicationTests.java
+++ web/src/test/java/com/recantoceuazul/web/WebApplicationTests.java
@@ -1,10 +1,10 @@
-package com.recantoceuazul.api;
+package com.recantoceuazul.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiApplicationTests {
+class WebApplicationTests {
 
 	@Test
 	void contextLoads() {
