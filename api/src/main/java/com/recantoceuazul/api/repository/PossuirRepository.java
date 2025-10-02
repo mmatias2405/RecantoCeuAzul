2d4cfdd1f9cfaa04859da27744bf1aff8bd95c30
mmatias2405 <matheusmatias555@gmail.com>
2025-10-02 17:17:28 -0300

    Remoção da classe referentes a tabela possuir

--- api/src/main/java/com/recantoceuazul/api/repository/PossuirRepository.java
+++
@@ -1,7 +0,0 @@
-package com.recantoceuazul.api.repository;
-
-import com.recantoceuazul.api.model.Possuir;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PossuirRepository extends JpaRepository<Possuir,Integer> {
-}
