fb1c2b844ba075df596f68bb3a0102da361dcdf6
mmatias2405 <matheusmatias555@gmail.com>
2025-11-10 09:21:48 -0300

    Inicio de integração das página estáticas com o backend

--- web/src/main/java/com/recantoceuazul/web/controller/HomeController.java
+++ web/src/main/java/com/recantoceuazul/web/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class HomeController {
 
@@ -13,6 +14,12 @@ public class HomeController {
     @GetMapping("/")
     public String index(Model model) {
         return "index";
+    }
+
+    @PostMapping("/auth")
+    public String auth(Model model, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("mensagem", "Teste de backend");
+        return "redirect:/";
     }
 
 }
