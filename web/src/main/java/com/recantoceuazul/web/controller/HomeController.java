7b6a3f65b8bd5862f8c6b1e6092c6cf4337bb359
mmatias2405 <matheusmatias555@gmail.com>
2025-11-10 11:13:58 -0300

    Página inicial funcional, com login mantendo sessão

--- web/src/main/java/com/recantoceuazul/web/controller/HomeController.java
+++ web/src/main/java/com/recantoceuazul/web/controller/HomeController.java
@@ -1,24 +1,101 @@
 package com.recantoceuazul.web.controller;
 
+import com.recantoceuazul.web.model.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.MediaType;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.http.HttpHeaders;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
-
+    
     @Value("${api.url}")
     private String API_URL;
-
+    private final RestTemplate restTemplate = new RestTemplate();
+    
     @GetMapping("/")
     public String index(Model model) {
         return "index";
     }
-
+    
+    @GetMapping("/cadastro")
+    public String cadastro(Model model) {
+        return "cadastro";
+    }
+    
     @PostMapping("/auth")
-    public String auth(Model model, RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("mensagem", "Teste de backend");
+    public String auth(@ModelAttribute Ator ator, RedirectAttributes redirectAttributes, HttpSession session) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Ator> request = new HttpEntity<>(ator, headers);
+        
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(API_URL + "ator/login", request, String.class);
+            
+            if (response.getStatusCode().is2xxSuccessful()) {
+                String id = response.getBody();
+                ResponseEntity<Ator> responseAtor = restTemplate.getForEntity(API_URL + "ator/" + id, Ator.class);
+                Ator atorLogado = responseAtor.getBody();
+                session.setAttribute("usuarioLogado", atorLogado);
+                if(atorLogado.getPapel().equals("ADMIN") || atorLogado.getPapel().equals("FISCA")){
+                    return "redirect:/dashboard";
+                }
+                return "redirect:/morador";
+            }
+        } catch (HttpClientErrorException e) {
+            if (e.getStatusCode().value() == 401) {
+                redirectAttributes.addFlashAttribute("mensagem", "Usuário ou senha inválidos.");
+                return "redirect:/"; 
+            }
+        }
+        
+        redirectAttributes.addFlashAttribute("mensagem", "Erro inesperado.");
+        return "redirect:/";
+    }
+    @GetMapping("/morador")
+    public String mostrarDashboardMorador(HttpSession session, Model model, RedirectAttributes redirectAttributes) {
+        Ator usuario = (Ator) session.getAttribute("usuarioLogado");
+        if (usuario == null) {
+            // Não está logado, redireciona para a home (login)
+            redirectAttributes.addFlashAttribute("mensagem", "Faça Login para acessar essa página!");
+            return "redirect:/";
+        }
+        if (usuario.getPapel().equals("ADMIN") || usuario.getPapel().equals("FISCA")){
+            return "redirect:/dashboard";
+        }
+        // 3. Se chegou aqui, ele está logado!
+        return "dashboardMorador"; // Retorna a página "dashboard.html" (por exemplo)
+    }
+    
+    @GetMapping("/dashboard")
+    public String mostrarDashboard(HttpSession session, Model model, RedirectAttributes redirectAttributes) {       
+        Ator usuario = (Ator) session.getAttribute("usuarioLogado");
+        if (usuario == null) {
+            // Não está logado, redireciona para a home (login)
+            redirectAttributes.addFlashAttribute("mensagem", "Faça Login para acessar essa página!");
+            return "redirect:/";
+        }
+    
+        if (usuario.getPapel().equals("MORAR") || usuario.getPapel() == null){
+            return "redirect:/morador";
+        }
+        
+        // 3. Se chegou aqui, ele está logado!
+        return "dashboardAdmin"; // Retorna a página "dashboard.html" (por exemplo)
+    }
+    
+    @GetMapping("/logout")
+    public String logout(HttpSession session, RedirectAttributes redirectAttributes) {
+        session.invalidate(); // Limpa/invalida a sessão
+        redirectAttributes.addFlashAttribute("mensagem", "Você saiu do sistema.");
         return "redirect:/";
     }
 
